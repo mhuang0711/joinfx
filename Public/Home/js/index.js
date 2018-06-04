71ae0f4d1c0f37ecda774e619e41158c176bc713
Xiaofang Huang <xiaofang@hpe.com>
2018-06-04 17:20:33 +0800

    optimize sixthPage

--- Public/Home/js/index.js
+++ Public/Home/js/index.js
@@ -170,7 +170,7 @@ $window.on("scroll resize", second_page_animation);
 $window.on("scroll resize", third_page_animation);
 $window.on("scroll resize", fourth_page_animation);
 $window.on("scroll resize", fifth_page_animation);
-$window.on("scroll resize", sixth_page_animation);
+// $window.on("scroll resize", sixth_page_animation);
 $window.trigger("scroll");
 
 
@@ -227,4 +227,48 @@ $("#prev").click(function () {
     $('#slide-2').removeClass('slide-closed').addClass("slide-childopened");
     $('#slide-3').removeClass('slide-closed').addClass('slide-opened');
   }
+});
+
+
+$('.form').find('input, textarea').on('keyup blur focus', function (e) {
+
+  var $this = $(this),
+    label = $this.prev('label');
+
+  if (e.type === 'keyup') {
+    if ($this.val() === '') {
+      label.removeClass('active highlight');
+    } else {
+      label.addClass('active highlight');
+    }
+  } else if (e.type === 'blur') {
+    if ($this.val() === '') {
+      label.removeClass('active highlight');
+    } else {
+      label.removeClass('highlight');
+    }
+  } else if (e.type === 'focus') {
+
+    if ($this.val() === '') {
+      label.removeClass('highlight');
+    } else if ($this.val() !== '') {
+      label.addClass('highlight');
+    }
+  }
+
+});
+
+$('.tab a').on('click', function (e) {
+
+  e.preventDefault();
+
+  $(this).parent().addClass('active');
+  $(this).parent().siblings().removeClass('active');
+
+  target = $(this).attr('href');
+
+  $('.tab-content > div').not(target).hide();
+
+  $(target).fadeIn(600);
+
 });
